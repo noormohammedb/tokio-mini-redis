5a52a883c6c1d1ef9e9f4649789a43037b2ecfc5
Noor Mohammed B <noormohammedb@protonmail.com>
2023-09-30 23:06:49 +0400

    basic listener

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+const PORT: u32 = 6379;
+const LOCALHOST: &str = "127.0.0.1";
 
 #[tokio::main]
-async fn main() -> Result<()> {
-  let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() -> () {
+  let lisn_socket_addr = format!("{LOCALHOST}:{PORT}");
+  let listener = TcpListener::bind(&lisn_socket_addr).await.unwrap();
 
-  client.set("hello", "world".into()).await?;
+  println!("server listening on: {lisn_socket_addr}");
 
-  let result = client.get("hello").await?;
+  loop {
+    let (socket, _) = listener.accept().await.unwrap();
+    process(socket).await;
+  }
+}
 
-  println!("got value from the server; result={:?}", result);
+async fn process(sockt: TcpStream) -> () {
+  let mut connection = Connection::new(sockt);
 
-  Ok(())
+  if let Some(frame) = connection.read_frame().await.unwrap() {
+    println!("GOT: {:?}", frame);
+
+    let response = Frame::Error("unimplemented".to_string());
+    connection.write_frame(&response).await.unwrap();
+  }
 }
