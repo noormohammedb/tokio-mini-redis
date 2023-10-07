d2556f016a564a136caf074a7300dd9002d53be9
Noor Mohammed <noormohammedb@protonmail.com>
2023-10-07 15:12:36 +0400

    mutichannel communication

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,6 +1,8 @@
 use bytes::Bytes;
 use mini_redis::client;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 const PORT: u32 = 6379;
 const LOCALHOST: &str = "127.0.0.1";
@@ -9,8 +11,15 @@ const LOCALHOST: &str = "127.0.0.1";
 async fn main() -> () {
   #[derive(Debug)]
   enum Command {
-    Get { key: String },
-    Set { key: String, val: Bytes },
+    Get {
+      key: String,
+      resp: Responder<Option<Bytes>>,
+    },
+    Set {
+      key: String,
+      val: Bytes,
+      resp: Responder<()>,
+    },
   }
   let server_socket_addr = format!("{LOCALHOST}:{PORT}");
 
@@ -25,11 +34,13 @@ async fn main() -> () {
       use Command::{Get, Set};
 
       match cmd {
-        Get { key } => {
-          let _ = client.get(&key).await;
+        Get { key, resp } => {
+          let res = client.get(&key).await;
+          resp.send(res).unwrap();
         }
-        Set { key, val } => {
-          let _ = client.set(&key, val).await;
+        Set { key, val, resp } => {
+          let res = client.set(&key, val).await;
+          resp.send(res).unwrap();
         }
       }
     }
@@ -38,16 +49,28 @@ async fn main() -> () {
   let tx2 = tx.clone();
 
   let t1 = tokio::spawn(async move {
-    let cmd = Command::Get { key: "foo".into() };
+    let (resp_tx, resp_rx) = oneshot::channel();
+    let cmd = Command::Get {
+      key: "foo".into(),
+      resp: resp_tx,
+    };
     let _ = tx.send(cmd).await.unwrap();
+
+    let res = resp_rx.await;
+    println!("GOT = {:?}", res);
   });
 
   let t2 = tokio::spawn(async move {
+    let (resp_tx, resp_rx) = oneshot::channel();
     let cmd = Command::Set {
       key: "foo".into(),
       val: "bar".into(),
+      resp: resp_tx,
     };
     let _ = tx2.send(cmd).await.unwrap();
+
+    let res = resp_rx.await;
+    println!("GOT = {:?}", res);
   });
 
   t1.await.unwrap();
