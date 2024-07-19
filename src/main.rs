e206af0202d635102c1d6054912a1109fad3f126
Noor Mohammed <noormohammedb@protonmail.com>
2024-07-19 15:21:25 +0400

    Hello Tokio
    basic client
    connection, set and get from crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect(("127.0.0.1", 6379)).await?;
+
+    client.set("Hello", "World".into()).await?;
+
+    let result = client.get("Hello").await?;
+
+    println!("Got value from the server; result={:?}", result);
+
+    Ok(())
 }
