8e2a797861bfe4d2d4db43bbdd297c0499980d42
Noor Mohammed <noormohammedb@protonmail.com>
2024-07-20 20:03:30 +0400

    spawning tokio task and dummy db for a connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+
+use mini_redis::{
+    Command::{self, Get, Set},
+    Connection, Frame,
+};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -7,17 +12,33 @@ async fn main() {
 
     loop {
         let (socket, _s_add) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
 async fn process(socket: TcpStream) {
+    let mut db = HashMap::new();
+
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let res = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(val) = db.get(cmd.key()) {
+                    Frame::Bulk(val.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
 
-        let res = Frame::Error("unimplemented".to_string());
         connection.write_frame(&res).await.unwrap();
     }
 }
