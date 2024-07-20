8d971281767202ef3d02139b8d2f7ea858075203
Noor Mohammed <noormohammedb@protonmail.com>
2024-07-21 01:59:51 +0400

    Shared state
    shared db for all the state wihich is arc mutex
    sharded db concept

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,47 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
+use bytes::Bytes;
 use mini_redis::{
     Command::{self, Get, Set},
     Connection, Frame,
 };
 use tokio::net::{TcpListener, TcpStream};
 
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
+type ShardedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind(("127.0.0.1", 6379)).await.unwrap();
 
+    let db = Arc::new(Mutex::new(HashMap::new()));
+    let _shared_db = new_sharded_db(8);
+
     loop {
-        let (socket, _s_add) = listener.accept().await.unwrap();
+        let (socket, s_add) = listener.accept().await.unwrap();
+        let db = db.clone();
+        println!("Accepted: {:?}", s_add);
+
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    let mut db = HashMap::new();
-
+async fn process(socket: TcpStream, db: Db) {
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let res = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(val) = db.get(cmd.key()) {
                     Frame::Bulk(val.clone().into())
                 } else {
@@ -41,4 +53,14 @@ async fn process(socket: TcpStream) {
 
         connection.write_frame(&res).await.unwrap();
     }
+}
+
+fn new_sharded_db(num_shards: usize) -> ShardedDb {
+    let mut db = Vec::with_capacity(num_shards);
+
+    for _ in 0..num_shards {
+        db.push(Mutex::new(HashMap::new()));
+    }
+
+    Arc::new(db)
 }
